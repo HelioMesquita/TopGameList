dd3829e908a776c12b68c6ae0da67634974f66b5
Helio Mesquita <helio.mesquitaios@gmail.com>
2018-07-18 23:19:31 -0300

    added list presenter test

---
+++ ironmanTests/New Group/ListPresenterTest.swift
@@ -0,0 +1,194 @@
+import Quick
+import Nimble
+
+@testable import ironman
+
+class ListPresenterTest: QuickSpec {
+
+  class DummyGameDB: GameDBDataStoreManager {
+
+    var hasSaved = false
+    var hasCleared = false
+    var hasRetrived = false
+
+    override func saveGames(games: [Game]) { hasSaved = true }
+    override func clearGames() { hasCleared = true}
+    override func retrieveGames() -> [Game] {
+      hasRetrived = true
+      return [Game(viewers: 0, channels: 0, imageUrl: URL(string: "www.google.com.br")!, name: "Iron Man")]
+    }
+  }
+
+
+  class DummyInteractor<S>: Interactor<S> where S: Decodable {
+    var isAPIResponseSuccess = true
+    var successModel: S
+    var errorModel: RequestError
+
+    init(successModel: S, errorModel: RequestError) {
+      self.successModel = successModel
+      self.errorModel = errorModel
+      let urlConfig = URLConfig(url: URL(string: "www.google.com.br/")!, queryParams: [])
+      super.init(urlConfig: urlConfig)
+    }
+
+    override func execute(onSuccess: @escaping (S) -> Void, onError: @escaping (RequestError) -> Void) {
+      if isAPIResponseSuccess {
+        onSuccess(successModel)
+      } else {
+        onError(errorModel)
+      }
+    }
+  }
+
+  class DummyListViewController: ListPresentable {
+    var hasLoaded = false
+    var hasPaginated = false
+    var preparedToLoad = false
+    var hasError = false
+    var refresherSet = false
+    var statedLoading = false
+    var endedLoading = false
+
+    func onLoad(list: GameList) { hasLoaded = true }
+    func onPaginate(list: GameList) { hasPaginated = true }
+    func prepareToLoadNextPage(url: URL) { preparedToLoad = true }
+    func onError(error: RequestError) { hasError = true }
+    func setRefresher() { refresherSet = true }
+    func startLoading() { statedLoading = true }
+    func endLoading() { endedLoading = true }
+  }
+
+  override func spec() {
+    super.spec()
+
+    let link = URL(string: "www.google.com.br")!
+    let game  = Game(viewers: 0, channels: 0, imageUrl: link, name: "Iron Man")
+    let gameList = GameList(links: Links(next: URL(string: "www.uol.com.br"), this: nil), list: [game])
+    let requestError = RequestError(error: "Loky appears", status: 0, message: "Fight")
+    var dummyViewController: DummyListViewController!
+    var interactor: DummyInteractor<GameList>!
+    var subject: ListPresenter!
+
+    beforeEach {
+      dummyViewController = DummyListViewController()
+      interactor = DummyInteractor<GameList>(successModel: gameList, errorModel: requestError)
+      subject = ListPresenter(interactor: interactor, delegate: dummyViewController, dataStore: DummyGameDB())
+    }
+
+    describe("#init") {
+      context("when initialize the presenter") {
+        it("returns true") {
+          expect(dummyViewController.refresherSet).to(beTrue())
+        }
+      }
+    }
+
+    describe("#performRequest") {
+      context("when the request is successfull on load is called") {
+        beforeEach {
+          interactor.isAPIResponseSuccess = true
+          subject.performRequest()
+        }
+        it("returns true") {
+          expect(dummyViewController.hasLoaded).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.statedLoading).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.endedLoading).to(beTrue())
+        }
+      }
+
+      context("when the request is unsuccessfull on error is called") {
+        beforeEach {
+          interactor.isAPIResponseSuccess = false
+          subject.performRequest()
+        }
+        it("returns true") {
+          expect(dummyViewController.hasError).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.statedLoading).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.endedLoading).to(beTrue())
+        }
+      }
+    }
+
+    describe("#performLoadData") {
+      context("returns games in core data") {
+        it("returns true") {
+          subject.performLoadData()
+
+          expect(dummyViewController.hasLoaded).to(beTrue())
+        }
+      }
+    }
+
+    describe("#handleInfinitScroll") {
+      context("with next link and row higher than 69% from total list") {
+        beforeEach {
+          subject.handleInfinitScroll(actualRow: 7, totalRows: 10, nextLink: link)
+        }
+        it("returns true") {
+          expect(dummyViewController.preparedToLoad).to(beTrue())
+        }
+      }
+
+      context("with next link and row lower than 69% from total list") {
+        beforeEach {
+          subject.handleInfinitScroll(actualRow: 6, totalRows: 10, nextLink: link)
+        }
+        it("returns false") {
+          expect(dummyViewController.preparedToLoad).to(beFalse())
+        }
+      }
+
+      context("without next link and row higher than 79% from total list") {
+        beforeEach {
+          subject.handleInfinitScroll(actualRow: 7, totalRows: 10, nextLink: nil)
+        }
+        it("returns false") {
+          expect(dummyViewController.preparedToLoad).to(beFalse())
+        }
+      }
+    }
+
+    describe("#presentNextPage") {
+      context("when the request is successfull on paginate is called") {
+        beforeEach {
+          interactor.isAPIResponseSuccess = true
+          subject.presentNextPage(nextInteractor: interactor)
+        }
+        it("returns true") {
+          expect(dummyViewController.hasPaginated).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.statedLoading).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.endedLoading).to(beTrue())
+        }
+      }
+
+      context("when the request is unsuccessfull on error is called") {
+        beforeEach {
+          interactor.isAPIResponseSuccess = false
+          subject.presentNextPage(nextInteractor: interactor)
+        }
+        it("returns true") {
+          expect(dummyViewController.statedLoading).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.endedLoading).to(beTrue())
+        }
+        it("returns true") {
+          expect(dummyViewController.hasError).to(beTrue())
+        }
+      }
+    }
+  }
+}
