87cb1db79cd935113cbe6e04e8f3dac2b52b67d3
Helio Mesquita <helio.mesquitaios@gmail.com>
2018-07-19 18:20:41 -0300

    added game image view component, improved colors, improve detail view controller

--- ironman/ConnectionStatus/ConnectionStatusAlert.swift
+++ ironman/ConnectionStatus/ConnectionStatusAlert.swift
@@ -4,7 +4,7 @@ import Whisper
 struct ConnectionStatusAlert {
 
   static func show() {
-    let murmur = Murmur(title: "No Connection", backgroundColor: #colorLiteral(red: 0.4039215686, green: 0.7803921569, blue: 0.9215686275, alpha: 1), titleColor: UIColor.white)
+    let murmur = Murmur(title: "No Connection", backgroundColor: #colorLiteral(red: 0.9843137255, green: 0.7921568627, blue: 0.01176470588, alpha: 1), titleColor: UIColor.black)
     Whisper.show(whistle: murmur, action: .present)
   }
 
--- ironman/Views/CardGameCollectionViewCell.swift
+++ ironman/Views/CardGameCollectionViewCell.swift
@@ -3,17 +3,13 @@ import UIKit
 class CardGameCollectionViewCell: UICollectionViewCell {
 
   @IBOutlet weak var titleLabel: UILabel!
-  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var imageView: GameImageView!
 
   override func awakeFromNib() {
     super.awakeFromNib()
     backgroundColor = UIColor.white
     clipsToBounds = true
     layer.cornerRadius = 10
-
-    titleLabel.textColor = UIColor.white
-    imageView.layer.cornerRadius = 10
-    imageView.clipsToBounds = true
   }
 
   func setCell(game: Game?) {
